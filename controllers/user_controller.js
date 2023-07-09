18feb12d395464b73e73de13348a31a9b0c3b968
Navneet Prajapati <navneetprajapati26@gmail.com>
2023-07-09 22:30:25 +0530

    chang login respons

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -59,5 +59,5 @@ export const login = async (req, res, next) => {
   if (!isPasswordCorrect) {
     return res.status(400).json({ message: "Incotrrect password" });
   }
-  return res.status(200).json({ message: "Login Successfull" });
+  return res.status(200).json({ existingUser });
 };
